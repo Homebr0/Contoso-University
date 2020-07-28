b5b836168a529f496d3b8c312b9159ab557670e3
Jason Davila <jsndavila1@gmail.com>
2020-07-28 11:39:43 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Contoso_University.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
