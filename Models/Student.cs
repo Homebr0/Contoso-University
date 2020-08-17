9c558ca52228efbfe1558cb43235a62fb084c800
Jason Davila <jsndavila1@gmail.com>
2020-08-17 12:26:00 -0400

    Modified details page and updated the create page to hide ID fields and added try/catch to deal with external errors

--- Models/Student.cs
+++ Models/Student.cs
@@ -9,7 +9,6 @@ namespace ContosoUniversity.Models
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
-
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
